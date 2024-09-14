c693e55c1dda00a951ca836b1252890cb0c7df62
Ayusht777 <talesarayush@gmail.com>
2024-09-14 18:59:46 +0530

    added hotel dashboard

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HotelDetailsComponent from "@/components/hotel-details";
+
+function App() {
+  return (
+    <>
+      <div className=" flex items-center justify-center h-full min-h-dvh">
+        <HotelDetailsComponent></HotelDetailsComponent>
+      </div>
+    </>
+  );
+}
+
+export default App;
