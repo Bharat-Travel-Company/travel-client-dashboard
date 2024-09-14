5277e896dce0521f149d21d69e06e44fd37ec2c6
divyanshu_sahu <divyanshusahucoc@gmail.com>
2024-09-15 00:53:35 +0530

    login and register done & getting error in functionslity so i commented that part coz i dont know typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import HotelDetailsComponent from "@/components/hotel-details";
+// import HotelDetailsComponent from "@/components/hotel-details";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
       <div className=" flex items-center justify-center h-full min-h-dvh">
-        <HotelDetailsComponent></HotelDetailsComponent>
+        {/* <HotelDetailsComponent></HotelDetailsComponent> */}
+        <Register/>
+        <Login/>
       </div>
     </>
   );
